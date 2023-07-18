0df465aea3307c6d7b024a8094ea8dbcf8ac831c
Eric Lau <ericglau@outlook.com>
2023-07-18 10:47:16 -0400

    test

--- script/MyToken.s.sol
+++ script/MyToken.s.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.9;
 
 import "forge-std/Script.sol";
 import "../src/MyToken.sol";
+import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 contract MyTokenScript is Script {
   function setUp() public {}
@@ -11,6 +12,18 @@ contract MyTokenScript is Script {
     vm.startBroadcast();
     MyToken instance = new MyToken();
     console.log("Contract deployed to %s", address(instance));
+
+    // ERC1967Proxy proxy = new ERC1967Proxy(address(instance), abi.encodeWithSelector(MyToken.initialize.selector, "hello"));
+   ERC1967Proxy proxy = new ERC1967Proxy(address(instance), abi.encodeCall(MyToken.initialize, "hello"));
+
+    MyToken p2 = MyToken(address(proxy));
+
+    console.log("Contract name: %s", p2.name());
+    console.log("greeting: %s", p2.greeting());
+    
+    vm.setEnv("myenv", "abc");
+    console.log("Getting env %s", vm.envString("myenv"));
+
     vm.stopBroadcast();
   }
 }
--- src/MyToken.sol
+++ src/MyToken.sol
@@ -5,17 +5,21 @@ import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 
 contract MyToken is Initializable, ERC20Upgradeable, OwnableUpgradeable, UUPSUpgradeable {
+    string public greeting;
+
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
     }
 
-    function initialize() initializer public {
+    function initialize(string memory _greeting) initializer public {
         __ERC20_init("MyToken", "MTK");
         __Ownable_init();
         __UUPSUpgradeable_init();
+        greeting = _greeting;
     }
 
     function _authorizeUpgrade(address newImplementation)
