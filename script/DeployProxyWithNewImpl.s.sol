3f320e55dc12501148a8a178aa9978d67aac1d6d
Eric Lau <ericglau@outlook.com>
2023-07-24 16:57:02 -0400

    fix broadcast

--- script/DeployProxyWithNewImpl.s.sol
+++ script/DeployProxyWithNewImpl.s.sol
@@ -21,7 +21,7 @@ contract MyTokenScript is Script {
     // Options opts = new Options();
     // opts.setUsePlatformDeploy(true);
 
-    vm.startBroadcast(0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80);
+    vm.startBroadcast();
     // MyToken v1 = new MyToken();
     // console.log("Impl: %s", address(v1));
     
